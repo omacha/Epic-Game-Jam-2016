bed54796efec954af5226913bd699cfd855dc569
JonasBaertschi <jonas.baertschi@gmail.com>
2016-07-02 00:22:56 +0200

    inventory beginning

---
+++ EMScape/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour {
+	GameObject cam;
+	// Use this for initialization
+	void Start () {
+		cam = GameObject.FindGameObjectWithTag ("MainCamera");
+		if (cam == null) {
+			Debug.Log ("Can't find the main Camera");
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 pos = cam.transform.position;
+		Vector3 thisPos = this.transform.position;
+		pos.x = thisPos.x;
+		cam.transform.position = pos;
+	}
+}
