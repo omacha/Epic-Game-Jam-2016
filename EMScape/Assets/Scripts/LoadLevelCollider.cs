b206f3711192542a04fc2f969b8dbcf2ba5a579f
Simon Narduzzi <simon.narduzzi@epfl.ch>
2016-07-02 02:19:29 +0200

    Added collider on doors, load level on collision

---
+++ EMScape/Assets/Scripts/LoadLevelCollider.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class LoadLevelCollider : MonoBehaviour {
+
+	public int levelToLoad = 1;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	void OnCollisionEnter(Collision collision) {
+		if (collision.gameObject.tag == "Player") {
+			SceneManager.LoadScene (levelToLoad);
+		}
+	}
+}
