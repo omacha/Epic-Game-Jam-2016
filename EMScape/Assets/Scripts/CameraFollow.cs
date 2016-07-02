851356dd5e7740dba30f7ebe1c2a0b34d27b4077
Robin Weiskopf <robin.weiskopf@gmail.com>
2016-07-02 02:40:34 +0200

    Adding camera following and player collision

--- EMScape/Assets/Scripts/CameraFollow.cs
+++ EMScape/Assets/Scripts/CameraFollow.cs
@@ -2,20 +2,50 @@
 using System.Collections;
 
 public class CameraFollow : MonoBehaviour {
-	GameObject cam;
+	public GameObject floor;
+	public float movementBuffer = 10.0f;
+	public float borderBuffer = 2.0f;
+
+	private Terrain terrain;
+	private GameObject cam;
+
 	// Use this for initialization
 	void Start () {
 		cam = GameObject.FindGameObjectWithTag ("MainCamera");
 		if (cam == null) {
-			Debug.Log ("Can't find the main Camera");
+			Debug.Log ("Can't find the GameObject MainCamera");
 		}
+
+		terrain = (Terrain) floor.GetComponent ("Terrain");
+		if (terrain == null) {
+			Debug.Log ("Floor has no \"Terrain\" Component.");
+		}
+
+		UpdateCameraPos ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		Vector3 pos = cam.transform.position;
-		Vector3 thisPos = this.transform.position;
-		pos.x = thisPos.x;
-		cam.transform.position = pos;
+		UpdateCameraPos ();
+	}
+
+	private void UpdateCameraPos() {
+		Vector3 terrainSize = terrain.terrainData.size;
+		Vector3 playerPos = this.transform.position;
+		Vector3 camPos = cam.transform.position;
+
+		// check player movement
+		if (Mathf.Abs (playerPos.x - camPos.x) > movementBuffer) {
+			camPos.x = playerPos.x - Mathf.Sign (playerPos.x - camPos.x) * movementBuffer;
+		}
+
+		// check world borders
+		if (camPos.x < floor.transform.position.x + borderBuffer) {
+			camPos.x = floor.transform.position.x + borderBuffer;
+		} else if (camPos.x > terrainSize.x - borderBuffer) {
+			camPos.x = terrainSize.x - borderBuffer;
+		}
+
+		cam.transform.position = camPos;
 	}
 }
--- EMScape/Assets/Scripts/Controller.cs
+++ EMScape/Assets/Scripts/Controller.cs
@@ -2,27 +2,59 @@
 using System.Collections;
 
 public class Controller : MonoBehaviour {
+	public float velocityUpDown = 10.0f;
+	public float velocityLeftRight = 10.0f;
+	public float borderPadding = 1.0f;
 
-	public float velocityUpDown = 0.8f;
-	public float velocityLeftRight = 0.1f;
+	public GameObject floor;
+	private Terrain terrain;
+
+	private float terrainXMin;
+	private float terrainXMax;
+	private float terrainZMin;
+	private float terrainZMax;
+
 	// Use this for initialization
 	void Start () {
-	
+		terrain = (Terrain)floor.GetComponent ("Terrain");
+		if (terrain == null) {
+			Debug.Log ("Floor has no \"Terrain\" Component.");
+		}
+
+		Vector3 terrainPosition = terrain.transform.position;
+		Vector3 terrainSize = terrain.terrainData.size;
+
+		terrainXMin = terrainPosition.x + borderPadding;
+		terrainXMax = terrainPosition.x + terrainSize.x - borderPadding;
+
+		terrainZMin = -terrainSize.z / 2 + borderPadding;
+		terrainZMax =  terrainSize.z / 2 - borderPadding;
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetKey (KeyCode.DownArrow)) {
-			this.transform.Translate (new Vector3 (0.0f, 0.0f, -1*velocityUpDown));
+		float upMvmt = Input.GetAxis ("Horizontal");
+		float sideMvmt = Input.GetAxis ("Vertical");
+
+		this.transform.Translate (new Vector3 (velocityUpDown * upMvmt * Time.deltaTime, 0.0f,  0.0f));
+		this.transform.Translate (new Vector3 (0.0f, 0.0f, velocityLeftRight * sideMvmt * Time.deltaTime));
+
+		// check for border collisions
+		Vector3 playerPos = this.transform.position;
+
+		if (playerPos.x < terrainXMin) {
+			playerPos.x = terrainXMin;
+		} else if (playerPos.x > terrainXMax) {
+			playerPos.x = terrainXMax;
 		}
-		if (Input.GetKey (KeyCode.UpArrow)) {
-			this.transform.Translate (new Vector3 (0.0f, 0.0f, 1*velocityUpDown));
+
+		if (playerPos.z < terrainZMin) {
+			playerPos.z = terrainZMin;
+		} else if (playerPos.z > terrainZMax) {
+			playerPos.z = terrainZMax;
 		}
-		if (Input.GetKey (KeyCode.RightArrow)) {
-			this.transform.Translate (new Vector3 (1*velocityLeftRight, 0.0f, 0.0f));
-		}
-		if (Input.GetKey (KeyCode.LeftArrow)) {
-			this.transform.Translate (new Vector3 (-1*velocityLeftRight, 0.0f, 0.0f));
-		}
+
+		this.transform.position = playerPos;
 	}
 }
