a8fda85cc5b89b762602d00c321cb8e390ce732f
Simon Narduzzi <simon.narduzzi@epfl.ch>
2016-07-03 16:21:31 +0200

    new project

---
+++ NEW/EMScape/Assets/Scripts/Weapons/AttackTrigger.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AttackTrigger : MonoBehaviour {
+
+	private float dmg = 2;
+	private AudioClip hitSound = null;
+
+	void OnTriggerEnter(Collider col) {
+		if (!col.isTrigger && col.CompareTag("Enemy") && col.GetComponent<Health>() != null) {
+			col.SendMessageUpwards ("Damage", dmg);
+			if (hitSound != null) {
+				AudioSource.PlayClipAtPoint (hitSound, transform.position);
+			}
+			GameObject.FindObjectOfType<Animator> ().SetBool ("AttackFryingPan", true);
+			GetComponent<BoxCollider> ().enabled = false;
+			Debug.Log (col.gameObject.name + " has received " + dmg + " damage!");
+		}
+	}
+
+	public void SetDamage(float dmg) {
+		this.dmg = dmg;
+	}
+
+	public void SetHitSound(AudioClip audio) {
+		hitSound = audio;
+	}
+
+}
